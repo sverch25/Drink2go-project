4ff226dd4c5f116464b239ed7568c1caba607d8b
sverch25 <zaichko@list.ru>
2024-06-17 11:45:20 +0500

    make html markup

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,10 @@
-/* в этот файл добавляет скрипты*/
+const sliderElement = document.querySelector('.catalog__price__slider');
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 1000,
+  },
+  start: [0, 900],
+  connect: [false, true, false],
+});
